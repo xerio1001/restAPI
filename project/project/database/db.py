02e592608c51601fe1118f5c11367cb8a5624edb
xerio1001 <dariovanhasselt@hotmail.be>
2022-04-22 16:14:59 +0200

    a

--- project/project/database/db.py
+++
@@ -1,6 +0,0 @@
-from flask_mongoengine import MongoEngine
-
-db = MongoEngine()
-
-def initiliaze_db(app):
-    db.init_app(app)
--- project/project/database/models/suppliers.py
+++
@@ -1,7 +0,0 @@
-from project.database.db import db
-
-class Supplier(db.Document):
-    supplier = db.StringField(required=True)
-    address = db.StringField(required=True)
-    phoneNumber = db.StringField(Required=True)
-    emailAddress = db.StringField(Required=True)
