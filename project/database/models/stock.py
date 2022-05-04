4945d38489b4da0bd326d9bd4e0f4f96245260f0
Dario Van Hasselt <dariovanhasselt@hotmail.be>
2022-05-04 10:41:10 +0200

    a

--- project/database/models/stock.py
+++ project/database/models/stock.py
@@ -3,7 +3,7 @@ from project.database.db import db
 class Stock(db.Document):
     name = db.StringField(required=True)
     brand = db.StringField(Required=False)
-    price = db.StringField(required=True)
+    price = db.DecimalField(required=True)
     amountInStock = db.StringField(required=True)
     amountPerOrder = db.StringField(required=True)
     supplier = db.StringField(required=True)
