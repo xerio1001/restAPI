2c6932976975d98de3e3fd4f629efa5e3acd1f32
xerio1001 <dariovanhasselt@hotmail.be>
2022-05-05 09:23:30 +0200

    a

--- project/database/models/stock.py
+++ project/database/models/stock.py
@@ -4,7 +4,7 @@ class Stock(db.Document):
     name = db.StringField(required=True)
     brand = db.StringField(Required=False)
     price = db.DecimalField(required=True)
-    unit = db.Stringfield(required=True)
+    unit = db.StringField(required=True)
     amountInStock = db.IntField(required=True)
     amountPerOrder = db.IntField(required=True)
     supplier = db.StringField(required=True)
